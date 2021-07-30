a82324668e6ce7351a707bcd0622eda433478f59
shendyppy <shendyppy@gmail.com>
2021-07-30 08:32:27 +0700

    fitur customer

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ const tagpage = require('./tags')
 const checkLogin = require('../middleware/checkLogin')
 
 router.use('/', startingpage)
-router.use('/seller',checkLogin, sellerpage)
-router.use('/customer',checkLogin, customerpage)
+router.use('/seller', checkLogin, sellerpage)
+router.use('/customer', checkLogin, customerpage)
 router.use('/tag', tagpage)
 
 module.exports = router
--- routes/povCustomer.js
+++ routes/povCustomer.js
@@ -5,6 +5,6 @@ const customerController = require('../controllers/customerController')
 router.get('/', customerController.getItems)
 router.get('/checkout/:id', customerController.getCheckOutItem)
 router.get('/checkout/:id/buy', customerController.getBuyCheckOutItem)
-router.get('/checkout/:id/delete', customerController.getDeleteCheckOutItem)
+// router.get('/checkout/', customerController.getCheckOut)
 
 module.exports = router
